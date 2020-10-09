28078a173cea1f6e6389a16d4850bb34f2fc56d6
TonyIbombot <71134792+TonyIbombot@users.noreply.github.com>
2020-10-09 15:39:36 +0200

    commentaire
    zzz

--- acceuil.php
+++ acceuil.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start();
-	
+	// COMMENTAIRE // 
 ?>
 
 <html>
@@ -55,7 +55,7 @@
         <p class="col-sm-12 col-md-12">Spécialiste de la vente et la location de tous les matériels, la vaisselle, les couverts et tous autres accessoires permettant de dresser une « Belle Table », Belle Table vous propose un catalogue complet de matériel en un seul prestataire :
         VAISSELLE, TABLES, CHAISES, LINGE DE TABLE, TENTES, MOBILIER LUMINEUX, MATERIEL DE CUISINE, …
 
-        Pour vos mariages, anniversaires, séminaires… Toute l’équipe de Belle Table est à votre service pour faire de votre événement professionnel ou personnel, une réussite.
+        Pour vos mariages, anniversaires, séminaires… Toute l’équipe de Belle Table est à votre service pour faire de votre événement professionnel ou personnel, une réussite. 
 
 </p>
       </div>
@@ -152,7 +152,7 @@
           $('html, body').animate({
             scrollTop: $(this.hash).offset().top
           }, 2000, function(){
-            window.location.hash = hash;
+            window.location.hash = hash; 
           });
         });
       });
